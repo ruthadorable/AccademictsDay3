59b6bae75c45a59cd966f885b8969cd6287b8d16
Michael Zumot <Michael.Zumot@inetum-realdolmen.world>
2022-09-05 16:12:05 +0200

    calculator michaelz

---
+++ groupProject/src/main/java/world/inetum/accademicts3/Calculator.java
@@ -0,0 +1,8 @@
+package world.inetum.accademicts3;
+
+public class Calculator {
+
+    public int addTwoNumbers(int a, int b) {
+        return a + b;
+    }
+}
--- groupProject/src/main/java/world/inetum/accademicts3/WeightOnMars.java
+++ groupProject/src/main/java/world/inetum/accademicts3/WeightOnMars.java
@@ -6,18 +6,6 @@ import java.util.Scanner;
 public class WeightOnMars {
 
 
-    public static void main(String[] args) {
-
-        try {
-            float weight = getWeight("Please provide your weight (on Earth:))");
-            float wightOnMars = yourWeightOnMars(weight);
-            System.out.println("your weight on planet Mars is " + wightOnMars);
-            System.out.println("==========================");
-        } catch (InputMismatchException e) {
-            System.out.println(e.getMessage());
-        }
-    }
-
     public static float yourWeightOnMars(float weight) throws InputMismatchException {
         if (weight < 1.0) {
             throw new InputMismatchException("Weight has to be at least 1.0KG or higher");
